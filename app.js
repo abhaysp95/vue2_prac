e1be5bbb68ed7a2ddc768f55f955fc0336050607
coolabhays <abhaysp9955@gmail.com>
2022-11-14 21:19:23 +0530

    feat: vuejs conditionals, v-if-else and v-show

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
 new Vue({
 	el: '#vue-app',
 	data: {
-		age: 20,
-		a: 0,
-		b: 0
+		error: true,
+		success: false
 	},
-	computed: {
-		addToA: function() {
-			return this.age + this.a
-		},
-		addToB: function() {
-			return this.age + this.b
-		}
-	},
-	/* methods: {
-		addToA: function() {
-			return this.age + this.a
-		},
-		addToB: function() {
-			return this.age + this.b
-		}
-	} */
 });
