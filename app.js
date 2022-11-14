46b349917c48a902fe97f9326fd476525bebfd91
coolabhays <abhaysp9955@gmail.com>
2022-11-14 21:46:59 +0530

    feat: vuejs looping and style/class binding

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 new Vue({
 	el: '#vue-app',
 	data: {
-		error: true,
-		success: false
+		current: false,
+		oldYonkous: [ "Shirohige", "Kaido", "Linlin", "Akagami" ],
+		newYonkous: [
+			{ name: "Akagami", bounty: "4.04B" },
+			{ name: "Kurohige", bounty: "3.6B" },
+			{ name: "Mugiwara", bounty: "3.0B" },
+			{ name: "Buggy", bounty: "3.0B" },
+		]
 	},
 });
