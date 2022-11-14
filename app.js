afa16acde41d5baba043f5d5578beb843a97ef4a
coolabhays <abhaysp9955@gmail.com>
2022-11-14 10:39:10 +0530

    feat: little on vue data binding

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ new Vue({
 	el: '#vue-app',
 	data: {
 		name: 'Someone',
-		job: 'Operator'
-	},
-	methods: {
-		greet: function(time) {
-			return `Good ${time}, ${this.name}`
-		}
+		job: 'Operator',
+		website: 'https://archlinux.org',
+		websiteTag: '<a href="https://v2.vuejs.org/v2/guide/">Visit Vue2</a>'
 	}
 });
