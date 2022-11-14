40244257477ac75e6ac2bde249dd9213e3463621
coolabhays <abhaysp9955@gmail.com>
2022-11-14 21:08:51 +0530

    feat: vuejs computed properties

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 new Vue({
 	el: '#vue-app',
 	data: {
-		name: "",
-		age: ""
+		age: 20,
+		a: 0,
+		b: 0
 	},
+	computed: {
+		addToA: function() {
+			return this.age + this.a
+		},
+		addToB: function() {
+			return this.age + this.b
+		}
+	},
+	/* methods: {
+		addToA: function() {
+			return this.age + this.a
+		},
+		addToB: function() {
+			return this.age + this.b
+		}
+	} */
 });
