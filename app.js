4bb1835d48d90cec5ee08ee217a52672be3652d0
coolabhays <abhaysp9955@gmail.com>
2022-11-15 14:19:57 +0530

    feat: vuejs using refs

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 new Vue({
-	el: '#vue-app'
+	el: '#vue-app',
+	data: {
+		favFood: "None"
+	},
+	methods: {
+		readRefs: function() {
+			// first line just for test
+			// console.log(`div test: ${this.$refs.getContent.innerHTML}`)
+			console.log(this.$refs.getContent)
+			this.favFood = this.$refs.userInput.value
+		}
+	}
 })
